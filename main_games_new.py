762a342cee7c0dc5061ac025509c6a61ded3b8ec
Giovanni Briglia <93917046+Giovannibriglia@users.noreply.github.com>
2024-01-09 16:15:19 +0100

    update

--- main_games_new.py
+++ main_games_new.py
@@ -11,11 +11,14 @@ import time
 
 # 'QL_EG', 'QL_SA', 'QL_BM', 'QL_TS' + all 'causal' + 'offline'/'online'
 # 'DQN' + 'causal'
-algorithms = ['QL_EG_basic', 'QL_EG_causal_offline', 'QL_EG_causal_online']
-n_games = 1
-vect_rows = [5]
-vect_n_enemies = [1]
-n_episodes = 5
+algorithms = ['QL_EG_basic', 'QL_EG_causal_offline', 'QL_EG_causal_online',
+              'QL_SA_basic', 'QL_SA_causal_offline', 'QL_SA_causal_online'
+              'QL_BM_basic', 'QL_BM_causal_offline', 'QL_BM_causal_online'
+              'QL_TS_basic', 'QL_TS_causal_offline', 'QL_TS_causal_online']
+n_games = 10
+vect_rows = [5, 10]
+vect_n_enemies = [2, 5, 10]
+n_episodes = 2500
 vect_if_maze = [False]
 vect_if_same_enemies_actions = [False]
 dir_start = f'Results_Baseline_Comp1_Comp2'
