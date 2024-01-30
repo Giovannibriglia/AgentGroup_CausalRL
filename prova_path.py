e97e8ce954e01a22c4e7287db8ed338a8e8247a1
Giovanni Briglia <93917046+Giovannibriglia@users.noreply.github.com>
2024-01-30 15:00:08 +0100

    update

--- prova_path.py
+++ prova_path.py
@@ -5,13 +5,13 @@ current_directory = os.getcwd()
 print("Current directory:", current_directory)
 
 try:
-   print(np.load('/home/gbriglia/CausalRL/Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
+   print(np.load('/home/gbriglia/AgentGroup_CausalRL/Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
 except:
     try:
-        print(np.load('gbriglia/CausalRL/Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
+        print(np.load('gbriglia/AgentGroup_CausalRL/Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
     except:
         try:
-            print(np.load('CausalRL/Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
+            print(np.load('AgentGroup_CausalRL/Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
         except:
             try:
                 print(np.load('Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
