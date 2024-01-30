c6ef759684920608e8a4628036a313f3e7349ded
Giovanni Briglia <93917046+Giovannibriglia@users.noreply.github.com>
2024-01-30 15:01:10 +0100

    update

--- prova_path.py
+++ prova_path.py
@@ -8,13 +8,13 @@ try:
    print(np.load('/home/gbriglia/AgentGroup_CausalRL/Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
 except:
     try:
-        print(np.load('gbriglia/AgentGroup_CausalRL/Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
+        print(np.load('/gbriglia/AgentGroup_CausalRL/Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
     except:
         try:
-            print(np.load('AgentGroup_CausalRL/Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
+            print(np.load('/AgentGroup_CausalRL/Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
         except:
             try:
-                print(np.load('Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
+                print(np.load('/Results_Comparison123/Grid/RandEnAct/5Enem/5x5/env_game5.npy'))
             except:
                 print('nothing')
 
