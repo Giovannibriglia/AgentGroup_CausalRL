f9f6adab6faa087fa8b12e76c73d56f98ad428ed
Giovanni Briglia <93917046+Giovannibriglia@users.noreply.github.com>
2024-04-08 09:04:54 +0200

    update

--- scripts/algorithms/__init__.py
+++ scripts/algorithms/__init__.py
@@ -1,6 +1,10 @@
+import sys
 
 from .causal_discovery import *
 from .dqn_agent import *
 from .q_learning_agent import *
 from .random_agent import *
+
+sys.path.append('../')
 from ...global_variables import *
+
