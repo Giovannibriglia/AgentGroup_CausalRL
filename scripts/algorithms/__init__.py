d6ca9db549659666124c02b6964cd0f3a4cbd800
Giovanni Briglia <93917046+Giovannibriglia@users.noreply.github.com>
2024-04-08 09:07:34 +0200

    update

--- scripts/algorithms/__init__.py
+++ scripts/algorithms/__init__.py
@@ -5,6 +5,6 @@ from .dqn_agent import *
 from .q_learning_agent import *
 from .random_agent import *
 
-sys.path.append('../')
-from ...global_variables import *
+sys.path.append('../CausalRL')
+from CausalRL.global_variables import *
 
